36805bc401b43b33c7fad0f9db4a0e5597a6bba9
flow1981 <florian.stagliano@gmx.de>
2019-10-08 14:52:47 +0100

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,18 @@
+class Person
+
+    def name=(persons_name)
+        @name=persons_name
+    end
+
+    def name
+        name=@name
+    end
+
+    def job=(type_of_job)
+        @job=type_of_job
+    end
+ 
+    def job
+        job=@job
+    end
+end
